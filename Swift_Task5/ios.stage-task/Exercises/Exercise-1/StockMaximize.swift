da82355dbbb9f5d64b31bed5997f77d318d261c4
Dmitry Suhoversky <dmitrysuhov@outlook.com>
2021-06-13 22:01:50 +0300

    feat: complete two tasks

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,20 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+
+        guard prices.count > 1 else {
+            return 0
+        }
+
+        var maxProfit = 0
+        var MutablePrices = prices
+        
+        while (MutablePrices.count>1) {
+            for _ in 0...MutablePrices.firstIndex(of: MutablePrices.max()!)! {
+                maxProfit += (MutablePrices.max()! - MutablePrices[0])
+                MutablePrices.remove(at: 0)
+            }
+        }
+    return maxProfit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,55 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+      
+        var mutableMessage = message
+        var result: String = ""
+                
+        while mutableMessage.count > 0 {
+            
+            var i = String(mutableMessage.prefix(while: {$0 != "["}))
+            if let multiplier = Int(i) {
+                
+                let firstRange = mutableMessage.range(of: i)
+                mutableMessage = mutableMessage.replacingCharacters(in: firstRange!, with: "")
+                        
+                let subMessage = SubMessage(of: mutableMessage)
+                let decryprted = decryptMessage(subMessage)
+                        
+                result.append(String(repeating: decryprted, count: multiplier))
+                        
+                let secondRange = mutableMessage.range(of: subMessage)
+                mutableMessage = mutableMessage.replacingCharacters(in: secondRange!, with: "")
+                } else {
+                
+                i = String(mutableMessage.first!)
+                if i != "]" && i != "[" {
+                    result.append(i)
+                }
+                mutableMessage = String(mutableMessage.dropFirst())
+            }
+        }
+                
+        return result
+    }
+    
+    private func SubMessage(of message: String) -> String {
+         
+         var subMessage = ""
+         var c = 0
+         
+         for char in message {
+            subMessage.append(char)
+            if char != "]" {
+            if char == "[" {
+            c += 1 }
+            } else {
+                c -= 1
+                if c == 0 {
+                    break
+                }
+            }
+        }
+        return subMessage
     }
 }
